fad1af8127a786e7b001cc8272e79462b4f96560
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2022-03-19 16:26:40 +0500

    Changed settings for deployment, created requirements.txtfile, runtime.txt, Procfile

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nrb-_xik&x^smkvqorw#r82v#jufq_(2r6o7z42&chbk#vx_ke'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['notes-frontent.herokuapp.com', '172.0.0.1']
 
 
 # Application definition
